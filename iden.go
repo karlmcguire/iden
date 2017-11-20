a2f2540ba7e7aa4457ac36e32750b80f2a2fe72e
Karl McGuire <karl@karlmcguire.com>
2017-11-20 18:12:52 -0500

    documentation

--- iden.go
+++ iden.go
@@ -1,3 +1,6 @@
+// Package iden implements secure identifiers. Idens are sent with requests,
+// allowing the server to check if the request is authenticated with a
+// specified key.
 package iden
 
 import (
@@ -6,23 +9,28 @@ import (
 	"encoding/hex"
 )
 
+// Iden is a byte slice (with a length of 32 bytes).
 type Iden []byte
 
-func NewIden(data, key []byte) Iden {
+// NewIden returns a new iden with the salt and key.
+func NewIden(salt, key []byte) Iden {
 	h := hmac.New(sha512.New, key)
-	h.Write(data)
+	h.Write(salt)
 	return Iden(h.Sum(nil))
 }
 
+// Equal checks if an iden is equal to the param iden.
 func (i Iden) Equal(iden Iden) bool {
 	return hmac.Equal(i, iden)
 }
 
+// EqualString checks if an iden is equal to the param iden (in string form).
 func (i Iden) EqualString(iden string) bool {
 	d, _ := hex.DecodeString(iden)
 	return hmac.Equal(i, d)
 }
 
+// String returns the iden in a hex-encoded format.
 func (i Iden) String() string {
 	return hex.EncodeToString(i)
 }
